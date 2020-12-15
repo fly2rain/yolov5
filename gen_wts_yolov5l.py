4800a8235818ea362efd9e338e122b860552aa50
Feiyun Zhu <Feiyun Zhu>
2020-12-15 14:32:22 -0600

    update 12152020

--- gen_wts_yolov5l.py
+++ gen_wts_yolov5l.py
@@ -5,15 +5,17 @@ from utils.torch_utils import select_device
 # Initialize
 device = select_device('cpu')
 # Load model
-model = torch.load('weights/yolov5s.pt', map_location=device)['model'].float()  # load to FP32
+
+# model = torch.load('weights/yolov5s.pt', map_location=device)['model'].float()  # load to FP32
+model = torch.load('runs/5_yolov5l_big_500epochs/weights/best.pt', map_location=device)['model'].float()
 model.to(device).eval()
 
-f = open('yolov5s.wts', 'w')
+f = open('runs/5_yolov5l_big_500epochs/weights/yolov5l.wts', 'w')
 f.write('{}\n'.format(len(model.state_dict().keys())))
 for k, v in model.state_dict().items():
     vr = v.reshape(-1).cpu().numpy()
     f.write('{} {} '.format(k, len(vr)))
     for vv in vr:
         f.write(' ')
-        f.write(struct.pack('>f',float(vv)).hex())
+        f.write(struct.pack('>f', float(vv)).hex())
     f.write('\n')
